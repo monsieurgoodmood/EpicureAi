af9ef8a55e8420a9b244e377155d83600acae5f0
Arthur Choisnet <arthur.choisnet74@gmail.com>
2023-11-30 00:04:37 +0100

    saving to my branch

--- EpicureAi_code/API_backend/main.py
+++ EpicureAi_code/API_backend/main.py
@@ -1,14 +1,13 @@
 from fastapi import FastAPI, UploadFile, File
-from fastapi.responses import FileResponse
+from fastapi.responses import JSONResponse
 from starlette.middleware.cors import CORSMiddleware
-
 import cv2
 import numpy as np
 from io import BytesIO
 
 app = FastAPI()
 
-# Configuration CORS pour permettre au frontend de communiquer avec l'API
+# Configuration CORS
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # Autorise toutes les origines
@@ -19,16 +18,17 @@ app.add_middleware(
 
 @app.post("/upload_image")
 async def upload_image(file: UploadFile = File(...)):
-    # Lire le fichier image
     contents = await file.read()
     nparr = np.frombuffer(contents, np.uint8)
     img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
 
-    # Traitement de l'image avec votre modèle (à implémenter)
-    # result = your_model.process_image(img)
+    # Ici, vous appellerez votre vrai modèle YOLO. Pour l'instant, nous utilisons une simulation
+    ingredients = mock_yolo_model(img)
 
-    # Sauvegarder l'image traitée temporairement (exemple)
-    output_filename = "temp_output_image.jpg"
-    cv2.imwrite(output_filename, img)  # Remplacer img par votre image traitée
+    # Retourner les ingrédients simulés
+    return JSONResponse(content={"ingredients": ingredients})
 
-    return FileResponse(output_filename)
+def mock_yolo_model(image):
+    # Cette fonction est juste un bouchon pour simuler la sortie de votre modèle YOLO
+    # Elle retourne une liste d'ingrédients fictifs
+    return ["ingrédient1", "ingrédient2", "ingrédient3"]
