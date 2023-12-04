28096bb6200848f1a65f08ce591ce6b35e4fbd0c
Joaquin Ortega <joaquinandresortega84@gmail.com>
2023-12-04 14:50:46 +0100

    as

--- epicureai/params.py
+++ epicureai/params.py
@@ -1,21 +1,6 @@
 import os
 
-# Model training
-DEVICE = "gpu"
-NUM_EPOCHS = 10
-IMGSZ = 512
 
-# Comet ML
-WEIGHTS = os.path.join(os.getcwd(),'comet_ml')
-
-# Data Augmentation
-BASE_DIRECTORY = os.path.join(os.getcwd(), 'EpicureAi.v12-balanced_data_set.yolov8')
-YAML_PATH = os.path.join(BASE_DIRECTORY,'data.yaml')
-
-# API Key
-ROBOFLOW_APIKEY = os.environ.get("ROBOFLOW_APIKEY")
-COMETML_APIKEY = os.environ.get("COMETML_APIKEY")
-OPENAI_KEY = os.environ.get("OPENAI_KEY")
 LOCAL_DATA_PATH = os.path.expanduser("~/.epicureai_data")
 NUM_EPOCHS = int(os.environ["NUM_EPOCHS"])
 COMET_API_KEY = os.environ["COMET_API_KEY"]
