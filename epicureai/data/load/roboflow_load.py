755d90c15fdc243a6af5442c04ba679859a1e340
Marcel Hoene <marcel_hoene@web.de>
2023-12-06 14:32:23 +0100

    tryinng with the roboflow augmented pictures

--- epicureai/data/load/roboflow_load.py
+++ epicureai/data/load/roboflow_load.py
@@ -5,8 +5,9 @@ from epicureai.params import *
 def load_data_from_roboflow():
     rf = Roboflow(api_key=ROBOFLOW_APIKEY)
     project = rf.workspace("wagon").project("epicureai")
-    dataset = project.version(13).download("yolov8", location=LOCAL_DATA_PATH)
+    dataset = project.version(14).download("yolov8", location=LOCAL_DATA_PATH)
 
 
+data=os.path.join(LOCAL_DATA_PATH,"data.yaml"),
 if __name__ == "__main__":
     load_data_from_roboflow()
--- epicureai/ml_logic/train.py
+++ epicureai/ml_logic/train.py
@@ -47,7 +47,7 @@ def train_model(epochs: int = 10, img_size: int = 512):
 
 
     model.train(
-        data=os.path.join(LOCAL_DATA_PATH,'augmented_data',"data.yaml"), #data=os.path.join(LOCAL_DATA_PATH,"data.yaml"), #
+        data=os.path.join(LOCAL_DATA_PATH,"data.yaml"), #data=os.path.join(LOCAL_DATA_PATH,'augmented_data',"data.yaml"),
         epochs=epochs,
         imgsz=img_size,
 
