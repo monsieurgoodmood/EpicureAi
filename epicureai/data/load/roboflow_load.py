2010979195819229ea294536e407eab17aaf5a0d
Marcel Hoene <marcel_hoene@web.de>
2023-12-07 13:47:33 +0100

    changed the robolfow version

--- epicureai/data/load/roboflow_load.py
+++ epicureai/data/load/roboflow_load.py
@@ -5,7 +5,8 @@ from epicureai.params import *
 def load_data_from_roboflow():
     rf = Roboflow(api_key=ROBOFLOW_APIKEY)
     project = rf.workspace("wagon").project("epicureai")
-    dataset = project.version(16).download("yolov8", location=LOCAL_DATA_PATH)
+    dataset = project.version(18).download("yolov8", location=LOCAL_DATA_PATH)
+
 
 
 data=os.path.join(LOCAL_DATA_PATH,"data.yaml"),
